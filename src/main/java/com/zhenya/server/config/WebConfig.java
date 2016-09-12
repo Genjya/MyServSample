6b584aa8590ae49eb6a6373ccceb3998e514eee7
genjya <jdo.genjya@gmail.com>
2016-09-12 16:32:56 +0300

    Controller and initializer are created

---
+++ src/main/java/com/zhenya/server/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.zhenya.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.zhenya.server")
+public class WebConfig extends WebMvcConfigurerAdapter{
+}
