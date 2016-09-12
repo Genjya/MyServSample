415bf4df5a975841df3e4ff75d1291320dabe8a4
genjya <jdo.genjya@gmail.com>
2016-09-12 19:04:12 +0300

    dataBase is deployed almost

--- src/main/java/com/zhenya/server/config/DatabaseConfig.java
+++ src/main/java/com/zhenya/server/config/DatabaseConfig.java
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
@@ -56,6 +58,21 @@ public class DatabaseConfig {
         ds.setDriverClassName(env.getProperty("db.driver"));
         ds.setUsername(env.getRequiredProperty("db.username"));
         ds.setPassword(env.getRequiredProperty("db.password"));
+
+        ds.setInitialSize(Integer.parseInt(env.getRequiredProperty("db.initial.size")));
+        ds.setMinIdle(Integer.parseInt(env.getRequiredProperty("db.minIdle")));
+        ds.setMaxIdle(Integer.parseInt(env.getRequiredProperty("db.maxIdle")));
+        ds.setTimeBetweenEvictionRunsMillis(Long.parseLong(env.getRequiredProperty("db.timeBetweenEvictionRunsMillis")));
+        ds.setMinEvictableIdleTimeMillis(Long.parseLong(env.getRequiredProperty("db.minEvictableIdleTimeMillis")));
+        ds.setTestOnBorrow(Boolean.parseBoolean(env.getRequiredProperty("db.testOnBarrow")));
+        ds.setValidationQuery(env.getRequiredProperty("db.valdationQuery"));
         return ds;
+    }
+
+    @Bean
+    public PlatformTransactionManager platformTransactionManager() {
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(entityManagerFactoryBean().getObject());
+        return manager;
     }
 }
